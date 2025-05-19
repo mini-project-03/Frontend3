6e0ae357f575ef06c52ca2c836f4cb5d21edbb14
qowldk <qowldk1030@gmail.com>
2025-05-19 14:16:02 +0900

    feat: 약속 시간에 분까지 표시되도록 formatTime 함수 수정 #77

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,6 +1,7 @@
 export const formatTime = (iso: string) => {
   const d = new Date(iso);
-  return `${d.getMonth() + 1}월 ${d.getDate()}일 ${d.getHours()}시`;
+  const pad = (n: number) => n.toString().padStart(2, '0');
+  return `${d.getMonth() + 1}월 ${d.getDate()}일 ${pad(d.getHours())}시 ${pad(d.getMinutes())}분`;
 };
 
 export const toDatetimeLocalFormat = (date: Date): string => {
